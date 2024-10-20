aeaa6e91a87924f63d60f993032b8bef81efe3c1
Liz Johnson <liz.j.johnson@oracle.com>
2024-10-20 10:35:00 -0700

    added source code and image examples produced

--- set-key.sh
+++ set-key.sh
@@ -1,2 +1,2 @@
-export TOGETHER_API_KEY="44f8859cda2b975bed9abf54bc2c6a0c7ce8522e914c8f7c2fa9190b295d2960"
-export LUMAAI_API_KEY="luma-0ca97f3c-ada4-4d2c-bdce-ab41375b1a1d-23ea2ecc-e792-4ed8-9b28-f5ad5642b50a"
+export TOGETHER_API_KEY=
+export LUMAAI_API_KEY=
